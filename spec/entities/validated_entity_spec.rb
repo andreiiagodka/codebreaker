f9fde06ec4815eb799fa32f541c3ff4a0b6d222b
andreiiagodka <andrei.iagodka@gmail.com>
2018-12-18 02:45:55 +0200

    added validated entity spec

---
+++ spec/entities/validated_entity_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+RSpec.describe ValidatedEntity do
+  subject(:validated_entity) { described_class.new }
+
+  let(:some_error) { 'Invalid input' }
+
+  describe '.new' do
+    it { expect(validated_entity.instance_variable_get(:@errors)).to eq([]) }
+  end
+
+  describe '#validate' do
+    it { expect { validated_entity.validate }.to raise_error(NotImplementedError) }
+  end
+end
