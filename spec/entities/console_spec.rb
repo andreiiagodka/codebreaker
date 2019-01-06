d18de441198aa582078202c5634ae054036fee7b
andreiiagodka <andrei.iagodka@gmail.com>
2019-01-06 04:23:13 +0200

    Add tests fro Console with testing output messages

--- spec/entities/console_spec.rb
+++ spec/entities/console_spec.rb
@@ -3,24 +3,45 @@
 RSpec.describe Console do
   subject(:console) { described_class.new }
 
-  let(:output) { Output.new }
-  let(:display) { output.display }
-  let(:introduction) { output.introduction }
+  let(:statistic) { Statistic.new }
 
-  let(:list_of_options) { Console::COMMANDS.values.map(&:capitalize) }
+  let(:list_of_options) { console.options_list.join("\n") }
+  let(:invalid_command) { Console::COMMANDS.keys }
 
   describe '#menu' do
-    it 'displays introduction message' do
-      allow(console).to receive(:introduction)
-    end
-
-    it 'displays options list' do
-      expect(console.options_list).to eq(list_of_options)
-      allow(console).to receive(:display).with(console.options_list)
-    end
-
-    it 'calls method to select option' do
+    before do
       allow(console).to receive(:select_option)
+      allow(console).to receive(:loop).and_yield
+    end
+
+    it 'outputs introduction message and list of options' do
+      expect { console.menu }.to output("#{I18n.t('message.introduction')}\n#{list_of_options}\n").to_stdout
+    end
+  end
+
+  describe '#select_option' do
+    before { allow(console).to receive(:navigation) }
+
+    after { console.send(:navigation) }
+
+    it 'when command #start' do
+      allow(console).to receive(:user_input).and_return(Console::COMMANDS[:start])
+      expect(console).to receive(:navigation)
+    end
+
+    it 'when command #rules' do
+      allow(console).to receive(:user_input).and_return(Console::COMMANDS[:rules])
+      expect { console.send(:select_option) }.to output("#{I18n.t('message.rules')}\n").to_stdout
+    end
+
+    it 'when command #stats' do
+      allow(console).to receive(:user_input).and_return(Console::COMMANDS[:stats])
+      expect { console.send(:select_option) }.to output("#{statistic.rating_table}\n").to_stdout
+    end
+
+    it 'when command is invalid' do
+      allow(console).to receive(:user_input).and_return(invalid_command)
+      expect { console.send(:select_option) }.to output("#{I18n.t('error.unexpected_option')}\n").to_stdout
     end
   end
 end
